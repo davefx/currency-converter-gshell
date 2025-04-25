be0053a7e46fa4978a5148174317887d819c03e5
David Marín <david.marin@toptal.com>
2025-04-25 10:27:16 +0200

    Uploading changes

--- prefs.js
+++ prefs.js
@@ -3,73 +3,98 @@
 import Gtk from 'gi://Gtk';
 import Gio from 'gi://Gio';
 import Soup from 'gi://Soup';
+import Adw from 'gi://Adw';
 
-let session;
-
-export function init() {}
-
-function fetchCurrencies(callback) {
-    if (!session) {
-        session = new Soup.Session();
+export default class CurrencyPrefs {
+    constructor() {
+        this._settings = new Gio.Settings({
+            schema_id: 'org.gnome.shell.extensions.currency-converter',
+        });
     }
 
-    const message = Soup.Message.new('GET', 'https://economia.awesomeapi.com.br/json/available/uniq');
+    fillPreferencesWindow(window) {
+        const page = new Adw.PreferencesPage();
+        const group = new Adw.PreferencesGroup({ title: 'Currency Settings' });
 
-    session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (source, res) => {
-        try {
-            let data = session.send_and_read_finish(res).get_data();
-            let json = JSON.parse(new TextDecoder().decode(data));
-            let codes = Object.keys(json).sort(); // Alphabetical order
-            callback(codes);
-        } catch (e) {
-            log(`Failed to fetch currency list: ${e}`);
-            callback(['USD', 'EUR']); // fallback
-        }
-    });
-}
+        // FROM currency
+        const fromRow = new Adw.ActionRow({ title: 'From currency', subtitle: 'Select source currency' });
+        this._fromCombo = new Gtk.ComboBoxText({ visible: true });
+        fromRow.add_suffix(this._fromCombo);
+        fromRow.activatable_widget = this._fromCombo;
 
-export function buildPrefsWidget() {
-    const settings = new Gio.Settings({ schema_id: 'org.gnome.shell.extensions.currency-converter' });
+        // TO currency
+        const toRow = new Adw.ActionRow({ title: 'To currency', subtitle: 'Select target currency' });
+        this._toCombo = new Gtk.ComboBoxText({ visible: true });
+        toRow.add_suffix(this._toCombo);
+        toRow.activatable_widget = this._toCombo;
 
-    const grid = new Gtk.Grid({
-        column_spacing: 12,
-        row_spacing: 12,
-        margin: 12,
-        visible: true,
-    });
-
-    const fromLabel = new Gtk.Label({ label: "From currency:", halign: Gtk.Align.START, visible: true });
-    const fromCombo = new Gtk.ComboBoxText({ visible: true });
-
-    const toLabel = new Gtk.Label({ label: "To currency:", halign: Gtk.Align.START, visible: true });
-    const toCombo = new Gtk.ComboBoxText({ visible: true });
-
-    // Add labels to grid now
-    grid.attach(fromLabel, 0, 0, 1, 1);
-    grid.attach(fromCombo, 1, 0, 1, 1);
-    grid.attach(toLabel, 0, 1, 1, 1);
-    grid.attach(toCombo, 1, 1, 1, 1);
-
-    fetchCurrencies((currencies) => {
-        currencies.forEach(c => {
-            fromCombo.append_text(c);
-            toCombo.append_text(c);
+        // Refresh button
+        const refreshButton = new Gtk.Button({
+            label: "Refresh currencies",
+            visible: true,
+            halign: Gtk.Align.CENTER,
+        });
+        refreshButton.connect('clicked', () => {
+            this._loadCurrencies();
         });
 
-        const source = settings.get_string('source-currency');
-        const target = settings.get_string('target-currency');
+        group.add(fromRow);
+        group.add(toRow);
+        page.add(group);
 
-        fromCombo.set_active(currencies.indexOf(source) !== -1 ? currencies.indexOf(source) : 0);
-        toCombo.set_active(currencies.indexOf(target) !== -1 ? currencies.indexOf(target) : 1);
+	const extraGroup = new Adw.PreferencesGroup();
+	const buttonBox = new Gtk.Box({
+	    orientation: Gtk.Orientation.VERTICAL,
+	    halign: Gtk.Align.CENTER,
+	    margin_top: 12,
+	    margin_bottom: 12,
+	    visible: true,
+	});
+	buttonBox.append(refreshButton);
+	extraGroup.add(buttonBox);
+	page.add(extraGroup); 
 
-        fromCombo.connect('changed', () => {
-            settings.set_string('source-currency', fromCombo.get_active_text());
+        window.add(page);
+        // Load currencies initially
+        this._loadCurrencies();
+    }
+
+    _loadCurrencies() {
+        const session = new Soup.Session();
+        const message = Soup.Message.new('GET', 'https://economia.awesomeapi.com.br/json/available/uniq');
+
+        session.send_and_read_async(message, 0, null, (source, result) => {
+            try {
+                const bytes = session.send_and_read_finish(result).get_data();
+                const json = JSON.parse(new TextDecoder().decode(bytes));
+                const currencies = Object.keys(json).sort();
+
+                // Clear previous entries
+                this._fromCombo.remove_all();
+                this._toCombo.remove_all();
+
+                for (const c of currencies) {
+                    this._fromCombo.append_text(c);
+                    this._toCombo.append_text(c);
+                }
+
+                const currentFrom = this._settings.get_string('source-currency');
+                const currentTo = this._settings.get_string('target-currency');
+
+                this._fromCombo.set_active(currencies.indexOf(currentFrom) !== -1 ? currencies.indexOf(currentFrom) : 0);
+                this._toCombo.set_active(currencies.indexOf(currentTo) !== -1 ? currencies.indexOf(currentTo) : 1);
+
+                this._fromCombo.connect('changed', () => {
+                    this._settings.set_string('source-currency', this._fromCombo.get_active_text());
+                });
+
+                this._toCombo.connect('changed', () => {
+                    this._settings.set_string('target-currency', this._toCombo.get_active_text());
+                });
+
+            } catch (e) {
+                log(`Currency fetch failed: ${e}`);
+            }
         });
-
-        toCombo.connect('changed', () => {
-            settings.set_string('target-currency', toCombo.get_active_text());
-        });
-    });
-
-    return grid;
+    }
 }
